b7ddbb9c3626a52e9b96162d3ff47f0a3dde96ee
YiRuYang <1287865814@qq.com>
2017-08-11 16:13:39 +0800

    8/11完成部分

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/8/8.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,25 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/8/8.
+ */
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    @SerializedName("update")
+    public Update update;
+
+    public class Update{
+
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
